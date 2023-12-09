3f0c3daef46f9cb227ff39642d4fdc2db3d4f70e
George Stephenson <gf.s@hotmail.com>
2023-12-09 14:02:19 +0000

    Day 1 Puzzle

---
+++ 2023/Day1/number-calculator.py
@@ -0,0 +1,15 @@
+import re
+
+sum = 0
+with open('2023/Day1/input.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        res = re.findall(r'\d', line)
+        print(res)
+        firstNumber = res[0]
+        lastNumber = res[-1]
+        print("First number " + firstNumber)
+        print("Last number " + lastNumber)
+        twoDigitNumber = firstNumber + lastNumber
+        sum += int(twoDigitNumber)
+print("Result " + str(sum))
