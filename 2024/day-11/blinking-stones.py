0cf5c45cea49e58c9dc242d8603f4cd9e676a6a2
George Stephenson <gf.s@hotmail.com>
2024-12-11 19:59:43 +0000

    Use functools.cache

--- 2024/day-11/blinking-stones.py
+++ 2024/day-11/blinking-stones.py
@@ -1,21 +1,15 @@
+import functools
 import re
 
 with open('2024/day-11/input.txt') as f:
     stones = list(map(int, re.findall(r'\d+', f.readlines()[0])))
 
-stone_iteration_map = {}
-
+@functools.cache
 def blink_stone(stone, iterations):
-    if stone in stone_iteration_map and iterations in stone_iteration_map[stone]:
-        return stone_iteration_map[stone][iterations]
-    elif stone not in stone_iteration_map:
-        stone_iteration_map[stone] = {}
     if stone == 0:
         if iterations == 1:
-            stone_iteration_map[stone][1] = 1
             return 1
-        stone_iteration_map[stone][iterations] = blink_stone(1, iterations-1)
-        return stone_iteration_map[stone][iterations]
+        return blink_stone(1, iterations-1)
     stone_str = str(stone)
     if len(stone_str) % 2 == 0:
         if iterations == 1:
@@ -23,13 +17,10 @@ def blink_stone(stone, iterations):
         half_stone_str_len = int(len(stone_str)/2)
         first_stone = int(stone_str[:half_stone_str_len])
         second_stone = int(stone_str[half_stone_str_len:])
-        stone_iteration_map[stone][iterations] = blink_stone(first_stone, iterations-1) + blink_stone(second_stone, iterations-1)
-        return stone_iteration_map[stone][iterations]
+        return blink_stone(first_stone, iterations-1) + blink_stone(second_stone, iterations-1)
     if iterations == 1:
-        stone_iteration_map[stone][1] = 1
         return 1
-    stone_iteration_map[stone][iterations] = blink_stone(stone*2024, iterations-1)
-    return stone_iteration_map[stone][iterations]
+    return blink_stone(stone*2024, iterations-1)
 
 blinks = 25
 count = 0
