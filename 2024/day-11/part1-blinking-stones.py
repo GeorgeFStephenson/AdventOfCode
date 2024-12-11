3a236182da53b6869b482afbea3aa0d73f01ebb0
George Stephenson <gf.s@hotmail.com>
2024-12-11 18:44:42 +0000

    2024 Day 11 Part 2

--- 2024/day-11/part1-blinking-stones.py
+++ 2024/day-11/part1-blinking-stones.py
@@ -3,26 +3,38 @@ import re
 with open('2024/day-11/input.txt') as f:
     stones = list(map(int, re.findall(r'\d+', f.readlines()[0])))
 
-blinks = 25
+stone_iteration_map = {}
 
-for _ in range(blinks):
-    original_stones = list(stones)
-    stones_added = 0
-    for idx, stone in enumerate(original_stones):
-        if stone == 0:
-            stones[idx+stones_added] = 1
-            continue
-        stone_str = str(stone)
-        if len(stone_str) % 2 == 0:
-            half_stone_str_len = int(len(stone_str)/2)
-            first_stone = int(stone_str[:half_stone_str_len])
-            second_stone = int(stone_str[half_stone_str_len:])
-            stones[idx+stones_added] = first_stone
-            stones.insert(idx+stones_added, second_stone)
-            stones_added += 1
-        else:
-            stones[idx+stones_added] = stone*2024
+def blink_stone(stone, iterations):
+    if stone in stone_iteration_map and iterations in stone_iteration_map[stone]:
+        return stone_iteration_map[stone][iterations]
+    elif stone not in stone_iteration_map:
+        stone_iteration_map[stone] = {}
+    if stone == 0:
+        if iterations == 1:
+            stone_iteration_map[stone][1] = 1
+            return 1
+        stone_iteration_map[stone][iterations] = blink_stone(1, iterations-1)
+        return stone_iteration_map[stone][iterations]
+    stone_str = str(stone)
+    if len(stone_str) % 2 == 0:
+        if iterations == 1:
+            return 2
+        half_stone_str_len = int(len(stone_str)/2)
+        first_stone = int(stone_str[:half_stone_str_len])
+        second_stone = int(stone_str[half_stone_str_len:])
+        stone_iteration_map[stone][iterations] = blink_stone(first_stone, iterations-1) + blink_stone(second_stone, iterations-1)
+        return stone_iteration_map[stone][iterations]
+    if iterations == 1:
+            stone_iteration_map[stone][1] = 1
+            return 1
+    stone_iteration_map[stone][iterations] = blink_stone(stone*2024, iterations-1)
+    return stone_iteration_map[stone][iterations]
 
-count = len(stones)
+blinks = 75
+
+count = 0
+for stone in stones:
+    count += blink_stone(stone, blinks)
 
 print("Result:", count)
